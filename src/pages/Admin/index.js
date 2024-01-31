77b6078e5bb874a8dcba9c5f90379e48b84b91f2
gualves <gustavo.allvess@icloud.com>
2024-01-31 13:42:40 -0300

    Criando funcionalidade de sigin e signout e gerenciamento da pagina ADMIN

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -1,9 +1,46 @@
+import { useState } from 'react'
 import './admin.css'
+import { auth } from '../../firebaseConnection'
+import { signOut } from 'firebase/auth'
 
 export default function Admin() {
+  const [tarefasInput, setTarefasInput] = useState('')
+
+  function handleRegister(e) {
+    e.preventDefault()
+  }
+
+  async function handleLogout() {
+    await signOut(auth)
+  }
+
   return (
-    <div>
-      <h1>Pagina ADMIN</h1>
+    <div className="admin-container">
+      <h1>Suas tarefas</h1>
+
+      <form className="form" onSubmit={handleRegister}>
+        <textarea
+          placeholder="Digite aqui suas tarefas..."
+          value={tarefasInput}
+          onChange={e => setTarefasInput(e.target.value)}
+        />
+
+        <button type="submit" className="btn-register">
+          Registrar tarefa
+        </button>
+      </form>
+
+      <article className="list">
+        <p>Estudar e praticar React</p>
+        <div>
+          <button>Editar tarefa</button>
+          <button className="btn-delete">Concluir</button>
+        </div>
+      </article>
+
+      <button className="btn-logout" onClick={handleLogout}>
+        Sair
+      </button>
     </div>
   )
 }
