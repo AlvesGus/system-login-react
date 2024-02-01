913757bb650484c41d90f4fb21a5b05c9106c7f1
gualves <gustavo.allvess@icloud.com>
2024-02-01 13:52:03 -0300

    Criando as  funcionalidades de cadastro de tarefas no admin

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -1,13 +1,78 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './admin.css'
-import { auth } from '../../firebaseConnection'
+import { auth, db } from '../../firebaseConnection'
 import { signOut } from 'firebase/auth'
+
+import {
+  addDoc,
+  collection,
+  onSnapshot,
+  query,
+  orderBy,
+  where
+} from 'firebase/firestore'
 
 export default function Admin() {
   const [tarefasInput, setTarefasInput] = useState('')
+  const [user, setUser] = useState({})
 
-  function handleRegister(e) {
+  const [tarefas, setTarefas] = useState([])
+
+  useEffect(() => {
+    async function loadTarefas() {
+      const userDetail = localStorage.getItem('@detailUser')
+      setUser(JSON.parse(userDetail))
+
+      if (userDetail) {
+        //buscando o conteudo dentro do nosso storage JSON e transformando em arquivo
+        const data = JSON.parse(userDetail)
+        //criando a  const referencia (para facilitar o caminho)
+        const tarefaRef = collection(db, 'tarefas')
+        //  criando nossa query, passando a referencia de caminho, ordenando pela categoria (created), ordenando a busca,
+        //  com a condição de userUid (do banco) é igual ao uid logado no "data" (OBS usasse '?' para não crashar a aplicacao)
+        const q = query(
+          tarefaRef,
+          orderBy('created', 'desc'),
+          where('userUid', '==', data?.uid)
+        )
+        const unsub = onSnapshot(q, snapshot => {
+          let lista = []
+
+          snapshot.forEach(doc => {
+            lista.push({
+              id: doc.id,
+              tarefa: doc.data().tarefa,
+              userUid: doc.data().userUid
+            })
+          })
+          console.log(lista)
+          setTarefas(lista)
+        })
+      }
+    }
+    loadTarefas()
+  }, [])
+
+  async function handleRegister(e) {
     e.preventDefault()
+
+    if (tarefasInput === '') {
+      alert('digite sua tarefa...')
+      return
+    }
+
+    await addDoc(collection(db, 'tarefas'), {
+      tarefa: tarefasInput,
+      created: new Date(),
+      userUid: user?.uid
+    })
+      .then(() => {
+        console.log('Tarefa cadastrada com sucesso ')
+        setTarefasInput('')
+      })
+      .catch(error => {
+        alert('Erro ao registrar')
+      })
   }
 
   async function handleLogout() {
