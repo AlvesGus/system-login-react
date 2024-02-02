139f4dc912890012893d616e4796ae7d656d051b
gualves <gustavo.allvess@icloud.com>
2024-02-02 12:48:31 -0300

    adicionando funcionalidade de edit e complete task

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -9,12 +9,16 @@ import {
   onSnapshot,
   query,
   orderBy,
-  where
+  where,
+  doc,
+  deleteDoc,
+  updateDoc
 } from 'firebase/firestore'
 
 export default function Admin() {
   const [tarefasInput, setTarefasInput] = useState('')
   const [user, setUser] = useState({})
+  const [edit, setEdit] = useState({})
 
   const [tarefas, setTarefas] = useState([])
 
@@ -61,6 +65,11 @@ export default function Admin() {
       return
     }
 
+    if (edit?.id) {
+      handleUpdate()
+      return
+    }
+
     await addDoc(collection(db, 'tarefas'), {
       tarefa: tarefasInput,
       created: new Date(),
@@ -79,6 +88,39 @@ export default function Admin() {
     await signOut(auth)
   }
 
+  async function deleteTarefa(id) {
+    const docRef = doc(db, 'tarefas', id)
+    await deleteDoc(docRef)
+      .then(() => {
+        alert('Tarefa concluida com sucesso')
+      })
+      .catch(error => {
+        alert('Ocorreu um erro ' + error)
+      })
+  }
+
+  function editarTarefa(item) {
+    setTarefasInput(item.tarefa)
+    setEdit(item)
+  }
+
+  async function handleUpdate() {
+    const docRef = doc(db, 'tarefas', edit?.id)
+
+    await updateDoc(docRef, {
+      tarefa: tarefasInput
+    })
+      .then(() => {
+        setTarefasInput('')
+        setEdit({})
+      })
+      .catch(() => {
+        alert('Erro ao atualizar')
+        setTarefasInput('')
+        setEdit({})
+      })
+  }
+
   return (
     <div className="admin-container">
       <h1>Suas tarefas</h1>
@@ -90,18 +132,31 @@ export default function Admin() {
           onChange={e => setTarefasInput(e.target.value)}
         />
 
-        <button type="submit" className="btn-register">
-          Registrar tarefa
-        </button>
+        {Object.keys(edit).length > 0 ? (
+          <button type="submit" className="btn-register">
+            Atualizar Tarefa
+          </button>
+        ) : (
+          <button type="submit" className="btn-register">
+            Registrar tarefa
+          </button>
+        )}
       </form>
 
-      <article className="list">
-        <p>Estudar e praticar React</p>
-        <div>
-          <button>Editar tarefa</button>
-          <button className="btn-delete">Concluir</button>
-        </div>
-      </article>
+      {tarefas.map(item => (
+        <article key={item.id} className="list">
+          <p>{item.tarefa}</p>
+          <div>
+            <button onClick={() => editarTarefa(item)}>Editar tarefa</button>
+            <button
+              onClick={() => deleteTarefa(item.id)}
+              className="btn-delete"
+            >
+              Concluir
+            </button>
+          </div>
+        </article>
+      ))}
 
       <button className="btn-logout" onClick={handleLogout}>
         Sair
